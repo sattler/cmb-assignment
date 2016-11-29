10b94e59dd6c5fcd1a76a81dcf06d5161df24e9c
Hannes Steinhäuser Home <h.steinhaeuser@gmx.de>
2016-11-29 11:14:31 +0100

    Removed redunant code

--- src/movement/MapRoomMovement.java
+++ src/movement/MapRoomMovement.java
@@ -25,25 +25,12 @@ import static movement.map.MapRoute.PINGPONG;
 
 /**
  * Map based movement model that uses predetermined paths within the map area.
+ * Predetermined paths are generated between point objects found in the map file.
  * Nodes using this model (can) stop on every route waypoint and find their
  * way to next waypoint using {@link DijkstraPathFinder}. There can be
  * different type of routes; see {@link #ROUTE_TYPE_S}.
  */
-public class MapRoomMovement extends MapRouteMovement implements
-        SwitchableMovement {
-
-    /** the Dijkstra shortest path finder */
-    private DijkstraPathFinder pathFinder;
-
-    /** Prototype's reference to all routes read for the group */
-    private List<MapRoute> allRoutes = null;
-    /** next route's index to give by prototype */
-    private Integer nextRouteIndex = null;
-    /** Index of the first stop for a group of nodes (or -1 for random) */
-    private int firstStopIndex = -1;
-
-    /** Route of the movement model's instance */
-    private MapRoute route;
+public class MapRoomMovement extends MapRouteMovement {
 
     /**
      * Creates a new movement model based on a Settings object's settings.
@@ -51,35 +38,17 @@ public class MapRoomMovement extends MapRouteMovement implements
      */
     public MapRoomMovement(Settings settings) {
         super(settings);
-        String fileName = settings.getSetting(ROUTE_FILE_S);
-        int type = settings.getInt(ROUTE_TYPE_S);
-        List<MapRoute> tempRoutes = MapRoute.readRoutes(fileName, type, getMap());
-        pathFinder = new DijkstraPathFinder(getOkMapNodeTypes());
-        allRoutes = generateRoomRoutes(tempRoutes, fileName, type, getMap());
-        nextRouteIndex = 0;
-        this.route = this.allRoutes.get(this.nextRouteIndex).replicate();
-        if (this.nextRouteIndex >= this.allRoutes.size()) {
-            this.nextRouteIndex = 0;
-        }
-
-        if (settings.contains(ROUTE_FIRST_STOP_S)) {
-            this.firstStopIndex = settings.getInt(ROUTE_FIRST_STOP_S);
-            if (this.firstStopIndex >= this.route.getNrofStops()) {
-                throw new SettingsError("Too high first stop's index (" +
-                        this.firstStopIndex + ") for route with only " +
-                        this.route.getNrofStops() + " stops");
-            }
-        }
+        allRoutes = generatePointRoutes(allRoutes, routeFileName, routeType, getMap());
+        initFirstIndex(settings);
     }
 
-    private List<MapRoute> generateRoomRoutes(List<MapRoute> tempRoutes, String fileName, int type, SimMap map) {
+    private List<MapRoute> generatePointRoutes(List<MapRoute> tempRoutes, String fileName, int type, SimMap map) {
 
         List<MapRoute> routes = new ArrayList<>();
-
         WKTReader reader = new WKTReader();
-        List<List<Coord>> coords;
         List<Coord> points;
-        File routeFile = null;
+        File routeFile;
+
         boolean mirror = map.isMirrored();
         double xOffset = map.getOffset().getX();
         double yOffset = map.getOffset().getY();
@@ -124,56 +93,4 @@ public class MapRoomMovement extends MapRouteMovement implements
         }
         return routes;
     }
-
-    /**
-     * Copyconstructor. Gives a route to the new movement model from the
-     * list of routes and randomizes the starting position.
-     * @param proto The MapRouteMovement prototype
-     */
-    protected MapRoomMovement(MapRoomMovement proto) {
-        super(proto);
-        this.route = proto.allRoutes.get(proto.nextRouteIndex).replicate();
-        this.firstStopIndex = proto.firstStopIndex;
-
-        if (firstStopIndex < 0) {
-			/* set a random starting position on the route */
-            this.route.setNextIndex(rng.nextInt(route.getNrofStops()-1));
-        } else {
-			/* use the one defined in the config file */
-            this.route.setNextIndex(this.firstStopIndex);
-        }
-
-        this.pathFinder = proto.pathFinder;
-
-        proto.nextRouteIndex++; // give routes in order
-        if (proto.nextRouteIndex >= proto.allRoutes.size()) {
-            proto.nextRouteIndex = 0;
-        }
-    }
-
-    @Override
-    public Path getPath() {
-        Path p = new Path(generateSpeed());
-        MapNode to = route.nextStop();
-
-        List<MapNode> nodePath = pathFinder.getShortestPath(lastMapNode, to);
-
-        // this assertion should never fire if the map is checked in read phase
-        assert nodePath.size() > 0 : "No path from " + lastMapNode + " to " +
-                to + ". The simulation map isn't fully connected";
-
-        for (MapNode node : nodePath) { // create a Path from the shortest path
-            p.addWaypoint(node.getLocation());
-        }
-
-        lastMapNode = to;
-
-        return p;
-    }
-
-    @Override
-    public MapRoomMovement replicate() {
-        return new MapRoomMovement(this);
-    }
-
 }
