3271402218274d0b1a1eaa4c00da2a0228e54934
Hannes <h.steinhaeuser@gmx.de>
2016-11-30 12:56:35 +0100

    Movement model update, lecture rooms

--- src/movement/MapRoomMovement.java
+++ src/movement/MapRoomMovement.java
@@ -6,13 +6,11 @@ package movement;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 import core.SettingsError;
 import input.WKTReader;
+import movement.helper.RandomHelper;
 import movement.map.DijkstraPathFinder;
 import movement.map.MapNode;
 import movement.map.MapRoute;
@@ -20,17 +18,29 @@ import core.Coord;
 import core.Settings;
 import movement.map.SimMap;
 
-import static movement.map.MapRoute.CIRCULAR;
-import static movement.map.MapRoute.PINGPONG;
-
 /**
  * Map based movement model that uses predetermined paths within the map area.
  * Predetermined paths are generated between point objects found in the map file.
  * Nodes using this model (can) stop on every route waypoint and find their
- * way to next waypoint using {@link DijkstraPathFinder}. There can be
- * different type of routes; see {@link #ROUTE_TYPE_S}.
+ * way to next waypoint using {@link DijkstraPathFinder}.
  */
-public class MapRoomMovement extends MapRouteMovement {
+public class MapRoomMovement extends MapBasedMovement {
+
+
+    public static final String MAP_ROOM_MOVEMENT_NS = "MapRoomMovement";
+    public static final String LECTURE_ROOM_MAP_FILE_S = "lectureRoomFile";
+    public static final String ROUTE_FILE_S = "routeFile";
+    public static final String ROUTE_TYPE_S = "routeType";
+    private String routeFileName;
+    private int routeType;
+    private DijkstraPathFinder pathFinder;
+    private List<MapRoute> allRoutes = null;
+
+    private Settings roomSettings;
+
+    private List<MapNode> roomNodes;
+
+    private RandomHelper randomHelper;
 
     /**
      * Creates a new movement model based on a Settings object's settings.
@@ -38,13 +48,32 @@ public class MapRoomMovement extends MapRouteMovement {
      */
     public MapRoomMovement(Settings settings) {
         super(settings);
-        allRoutes = generatePointRoutes(allRoutes, routeFileName, routeType, getMap());
-        initFirstIndex(settings);
+        RandomHelper.createInstance(rng);
+        randomHelper = RandomHelper.getInstance();
+        roomSettings = new Settings(MAP_ROOM_MOVEMENT_NS);
+        String lrMapFileName = roomSettings.getSetting(LECTURE_ROOM_MAP_FILE_S);
+        routeFileName = settings.getSetting(ROUTE_FILE_S);
+        routeType = settings.getInt(ROUTE_TYPE_S);
+        allRoutes = MapRoute.readRoutes(routeFileName, routeType, getMap());
+        pathFinder = new DijkstraPathFinder(getOkMapNodeTypes());
+        roomNodes = generatePointRoutes(allRoutes, routeFileName, lrMapFileName, getMap());
     }
 
-    private List<MapRoute> generatePointRoutes(List<MapRoute> tempRoutes, String fileName, int type, SimMap map) {
+    /**
+     * Copyconstructor.
+     * @param proto The MapRouteMovement prototype
+     */
+    protected MapRoomMovement(MapRoomMovement proto) {
+        super(proto);
 
-        List<MapRoute> routes = new ArrayList<>();
+        this.randomHelper = proto.randomHelper;
+        this.pathFinder = proto.pathFinder;
+        this.allRoutes = proto.allRoutes;
+        this.roomNodes = proto.roomNodes;
+    }
+
+    private List<MapNode> generatePointRoutes(List<MapRoute> tempRoutes, String fileName, String lrFileName, SimMap map) {
+
         WKTReader reader = new WKTReader();
         List<Coord> points;
         File routeFile;
@@ -53,12 +82,8 @@ public class MapRoomMovement extends MapRouteMovement {
         double xOffset = map.getOffset().getX();
         double yOffset = map.getOffset().getY();
 
-        if (type != CIRCULAR && type != PINGPONG) {
-            throw new SettingsError("Invalid route type (" + type + ")");
-        }
-
         try {
-            routeFile = new File(fileName);
+            routeFile = new File(lrFileName);
             points = reader.readPoints(routeFile);
         }
         catch (IOException ioe){
@@ -73,24 +98,65 @@ public class MapRoomMovement extends MapRouteMovement {
             point.translate(xOffset, yOffset);
         }
 
-        HashMap<Coord, MapNode> roomMapNode = new HashMap<>();
+        Set<MapNode> roomNodes = new HashSet<>();
         for (MapRoute route : tempRoutes) {
             for (MapNode node : route.getStops()) {
                 for (Coord point : points) {
                     if (node.getLocation().equals(point)) {
-                        roomMapNode.put(point, node);
+                        roomNodes.add(node);
                     }
                 }
             }
         }
 
-        for (MapNode nOutside : roomMapNode.values()){
-            for (MapNode nInside : roomMapNode.values()){
-                if (!nOutside.equals(nInside)) {
-                    routes.add(new MapRoute(type, pathFinder.getShortestPath(nOutside, nInside)));
-                }
-            }
-        }
-        return routes;
+        return new ArrayList<>(roomNodes);
     }
+
+    @Override
+    public Path getPath() {
+        Path p = new Path(generateSpeed());
+
+        MapNode to = roomNodes.get(randomHelper.getRandomIntBetween(0, roomNodes.size()));
+
+        List<MapNode> nodePath = pathFinder.getShortestPath(lastMapNode, to);
+
+        // this assertion should never fire if the map is checked in read phase
+        assert nodePath.size() > 0 : "No path from " + lastMapNode + " to " +
+                to + ". The simulation map isn't fully connected";
+
+        for (MapNode node : nodePath) { // create a Path from the shortest path
+            p.addWaypoint(node.getLocation());
+        }
+
+        lastMapNode = to;
+
+        return p;
+    }
+
+    /**
+     * Returns the first stop on the route which is one of the lecture rooms
+     */
+    @Override
+    public Coord getInitialLocation() {
+        if (lastMapNode == null) {
+            lastMapNode = roomNodes.get(randomHelper.getRandomIntBetween(0, roomNodes.size()));
+        }
+
+        return lastMapNode.getLocation().clone();
+    }
+
+    @Override
+    public Coord getLastLocation() {
+        if (lastMapNode != null) {
+            return lastMapNode.getLocation().clone();
+        } else {
+            return null;
+        }
+    }
+    
+    @Override
+    public MapRoomMovement replicate() {
+        return new MapRoomMovement(this);
+    }
+
 }
